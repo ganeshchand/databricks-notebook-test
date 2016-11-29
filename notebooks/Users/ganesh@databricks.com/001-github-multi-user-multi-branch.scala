e622f6fbeb7c81ab2ad810a58525ee42937f6f4b
Giri R Varatharajan <giri.varatharajan@gmail.com>
2016-11-28 16:29:54 -0800

    Update 001-github-multi-user-multi-branch.scala

--- notebooks/Users/ganesh@databricks.com/001-github-multi-user-multi-branch.scala
+++ notebooks/Users/ganesh@databricks.com/001-github-multi-user-multi-branch.scala
@@ -1,5 +1,13 @@
-// Databricks notebook source exported at Tue, 29 Nov 2016 00:24:18 UTC
+// Databricks notebook source exported at Tue, 29 Nov 2016 00:29:54 UTC
 s"Hello World"
+
+// COMMAND ----------
+
+"Working on a new branch"
+
+// COMMAND ----------
+
+"This is test3"
 
 // COMMAND ----------
 
