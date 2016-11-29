8dc08244658dc245f1af0a748206f9a50136b9b1
Ganesh Chand <ganeshchand@gmail.com>
2016-11-28 16:24:19 -0800

    Update 001-github-multi-user-multi-branch.scala

--- notebooks/Users/ganesh@databricks.com/001-github-multi-user-multi-branch.scala
+++ notebooks/Users/ganesh@databricks.com/001-github-multi-user-multi-branch.scala
@@ -1,4 +1,4 @@
-// Databricks notebook source exported at Tue, 29 Nov 2016 00:23:28 UTC
+// Databricks notebook source exported at Tue, 29 Nov 2016 00:24:18 UTC
 s"Hello World"
 
 // COMMAND ----------
