a02c2927d65776f8af412ac7dcadd0ab9830bb97
Ganesh Chand <ganeshchand@gmail.com>
2017-02-23 15:23:15 -0800

    initial commit

---
+++ notebooks/github-test-notebook.scala
@@ -0,0 +1,8 @@
+// Databricks notebook source
+// MAGIC %md
+// MAGIC 
+// MAGIC #Github integration test:
+// MAGIC  Testing same notebook being updated on two separate branches
+
+// COMMAND ----------
+
