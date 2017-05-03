cd28710d01ae6b701a29b9a97016759a433e7d85
Ganesh Chand <ganeshchand@gmail.com>
2017-05-03 11:43:43 -0700

    iniital commit - notebooks contains cells with titles

---
+++ notebooks/01-notebook-with-cell-titles.scala
@@ -0,0 +1,14 @@
+// Databricks notebook source
+// MAGIC %md
+// MAGIC ## This is a notebook containing cell titles and to test the impact on cell titles when external changes are made to the notebook via git 
+
+// COMMAND ----------
+
+def sayHelloTo(name: String) = s"Hello $name"
+
+// COMMAND ----------
+
+sayHelloTo("Ganesh")
+
+// COMMAND ----------
+
